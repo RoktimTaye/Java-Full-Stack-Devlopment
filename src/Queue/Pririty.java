c4be3dff0b68b5628ca7eb239aefb3482ce36e4b
Roktim Taye <roktimtayecoding@gmail..com>
2025-08-22 22:52:34 +0530

    First commit Aug 22 2

---
+++ src/Queue/Pririty.java
@@ -0,0 +1,26 @@
+package Queue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Pririty {
+    public static void main(String[] args) {
+        Queue<String> MyQueue = new PriorityQueue<>();
+        MyQueue.add("Roktim");
+        MyQueue.add("Rohan");
+        MyQueue.add("Raj");
+        MyQueue.add("Raman");
+        MyQueue.add("Rahul");
+        MyQueue.add("Ramit1");
+        MyQueue.add("Rohan1");
+        MyQueue.add("Raj1");
+        MyQueue.add("Raman1");
+        MyQueue.add("Rahul1");
+        MyQueue.add("Ramit1");
+
+        System.out.println("Name List : "+MyQueue);
+
+        System.out.println("Name List : "+MyQueue);
+
+    }
+}
